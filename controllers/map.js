2087790f961e8dc028f2916c53e42bea7dfe4c56
wilyfreddie <garanivan59@gmail.com>
2020-10-07 14:38:10 +0800

    removed console log at map endpoint

--- controllers/map.js
+++ controllers/map.js
@@ -14,8 +14,8 @@ exports.returnAll = async (req, res) => {
     RAFT.findAll({
       raw: true,
     }).then(function (raft) {
-      console.log(report);
-      console.log(raft);
+      // console.log(report);
+      // console.log(raft);
       res.status(200).json({
         mobile: report,
         raft: raft,
