daa19251047ca9481d6a1ad0fb86f2179303b1c1
wilyfreddie <garanivan59@gmail.com>
2020-10-28 10:44:30 +0800

    fixed charts API and added new markers

--- controllers/raft.js
+++ controllers/raft.js
@@ -3,7 +3,7 @@ const Op = Sequelize.Op;
 
 const { Pool, Client } = require("pg");
 
-const client = new Client({
+const pool = new Pool({
   user: "tammy",
   host: "RainFLOW.live",
   database: "actorcloud",
@@ -12,30 +12,30 @@ const client = new Client({
 });
 
 exports.returnCharts = async (req, res) => {
-  client
-    .connect()
-    .then(async () => {
-      console.log("Successfully connected to ActorCloud.");
-      // var sql = `SELECT * FROM devices where "deviceID" = '${req.params.deviceID}' AND "showOnMap" = 1`;
-      var sql_FD1 = `select data.value->'time'  as time, data.value->'value' as value from "device_events", jsonb_each(device_events.data) AS data  where "deviceID" = '${req.params.deviceID}' and data.key = 'FD1' and "msgTime" > current_date - interval '1' day;`;
-      var sql_TMP1 = `select data.value->'time' as time, data.value->'value' as value from "device_events", jsonb_each(device_events.data) AS data  where "deviceID" = '${req.params.deviceID}' and data.key = 'TMP1' and "msgTime" > current_date - interval '1' day;`;
-      var sql_RA1 = `select data.value->'time' as time, data.value->'value' as value from "device_events", jsonb_each(device_events.data) AS data  where "deviceID" = '${req.params.deviceID}' and data.key = 'RA1' and "msgTime" > current_date - interval '1' day;`;
-      var sql_PR1 = `select data.value->'time' as time, data.value->'value' as value from "device_events", jsonb_each(device_events.data) AS data  where "deviceID" = '${req.params.deviceID}' and data.key = 'PR1' and "msgTime" > current_date - interval '1' day;`;
-      var sql_HU1 = `select data.value->'time' as time, data.value->'value' as value from "device_events", jsonb_each(device_events.data) AS data  where "deviceID" = '${req.params.deviceID}' and data.key = 'HU1' and "msgTime" > current_date - interval '1' day;`;
-      var FD1 = await client.query(sql_FD1);
-      var TMP1 = await client.query(sql_TMP1);
-      var RA1 = await client.query(sql_RA1);
-      var PR1 = await client.query(sql_PR1);
-      var HU1 = await client.query(sql_HU1);
-      res.status(200).json({
-        FD1: FD1.rows,
-        TMP1: TMP1.rows,
-        RA1: RA1.rows,
-        PR1: PR1.rows,
-        HU1: HU1.rows,
-      });
+  // client
+  //   .connect()
+  //   .then(async () => {
+  // console.log("Successfully connected to ActorCloud.");
+  // var sql = `SELECT * FROM devices where "deviceID" = '${req.params.deviceID}' AND "showOnMap" = 1`;
+  var sql_FD1 = `select data.value->'time'  as time, data.value->'value' as value from "device_events", jsonb_each(device_events.data) AS data  where "deviceID" = '${req.params.deviceID}' and data.key = 'FD1' and "msgTime" > current_date - interval '1' day;`;
+  var sql_TMP1 = `select data.value->'time' as time, data.value->'value' as value from "device_events", jsonb_each(device_events.data) AS data  where "deviceID" = '${req.params.deviceID}' and data.key = 'TMP1' and "msgTime" > current_date - interval '1' day;`;
+  var sql_RA1 = `select data.value->'time' as time, data.value->'value' as value from "device_events", jsonb_each(device_events.data) AS data  where "deviceID" = '${req.params.deviceID}' and data.key = 'RA1' and "msgTime" > current_date - interval '1' day;`;
+  var sql_PR1 = `select data.value->'time' as time, data.value->'value' as value from "device_events", jsonb_each(device_events.data) AS data  where "deviceID" = '${req.params.deviceID}' and data.key = 'PR1' and "msgTime" > current_date - interval '1' day;`;
+  var sql_HU1 = `select data.value->'time' as time, data.value->'value' as value from "device_events", jsonb_each(device_events.data) AS data  where "deviceID" = '${req.params.deviceID}' and data.key = 'HU1' and "msgTime" > current_date - interval '1' day;`;
+  var FD1 = await pool.query(sql_FD1);
+  var TMP1 = await pool.query(sql_TMP1);
+  var RA1 = await pool.query(sql_RA1);
+  var PR1 = await pool.query(sql_PR1);
+  var HU1 = await pool.query(sql_HU1);
+  res.status(200).json({
+    FD1: FD1.rows,
+    TMP1: TMP1.rows,
+    RA1: RA1.rows,
+    PR1: PR1.rows,
+    HU1: HU1.rows,
+  });
 
-      await client.end();
-    })
-    .catch((err) => console.error("Connection error", err.stack));
+  //   await client.end();
+  // })
+  // .catch((err) => console.error("Connection error", err.stack));
 };
